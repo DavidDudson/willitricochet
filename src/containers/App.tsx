7e7cf6c111e3d59ad83a9640cea34483fd284779
David Dudson <davidjohndudson@gmail.com>
2017-06-22 17:46:22 +1200

    Move App component to containers

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,7 +1,6 @@
 import * as React from "react";
 
 export default class App extends React.Component<any, any> {
-
     render() {
         return (
             <div>
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { AppContainer } from "react-hot-loader";
-import App from "./App";
+import App from "./containers/App";
 
 const rootEl = document.getElementById("root");
 ReactDOM.render(
@@ -13,7 +13,7 @@ ReactDOM.render(
 
 // Hot Module Replacement API
 if (module.hot) {
-  module.hot.accept("./App", () => {
+  module.hot.accept("./containers/App", () => {
     const NextApp = require<RequireImport>("./App").default;
     ReactDOM.render(
       <AppContainer>
